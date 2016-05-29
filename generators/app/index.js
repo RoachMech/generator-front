556117074119727b52302c351c0e1f74e5e814a6
Roach <roach@MBP-van-Roach.home>
2016-05-29 22:34:31 +0200

    refactoring

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,33 +2,18 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
-var mkdirp = require('mkdirp');
 
-module.exports = yeoman.Base.extend({
-    prompting: function () {
-        // Have Yeoman greet the user.
+var FrontGenerator = yeoman.Base.extend({
+
+    hi: function () {
+
+        // Say hi
         this.log(yosay(
-            'Scaffolding a nice project structure'
+            'Hi, I will scaffold a nice project structure for you'
         ));
 
-        var prompts = [{
-            name: 'name',
-            message: 'How would you like to call your new project?'
-        }];
-
-        return this.prompt(prompts).then(function (props) {
-            // To access props later use this.props.someAnswer;
-            this.props = props;
-        }.bind(this));
-    },
-
-    writing: function () {
-        this.log(this.props.name);
-        mkdirp.sync(this.props.name);
-        // this.fs.write(this.props.name);
-    },
-
-    install: function () {
-        // this.installDependencies();
     }
+
 });
+
+module.exports = FrontGenerator;
