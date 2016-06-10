ba9ba805f7c91ed7152b5c32a5bda49334d63562
Roach <roach@MBP-van-Roach.home>
2016-06-10 14:44:48 +0200

    copy _gulpfile.js

--- generators/app/src/scaffold.js
+++ generators/app/src/scaffold.js
@@ -9,7 +9,7 @@ module.exports = function (FrontGenerator) {
         this.bulkDirectory('assets', 'assets');
         this.bulkDirectory('gulp', 'gulp');
         this.copy('public_html/_index.html', 'public_html/index.html');
-        this.copy('_gulpfile.json', 'gulpfile.json');
+        this.copy('_gulpfile.js', 'gulpfile.js');
         this.copy('_package.json', 'package.json');
         this.copy('_bower.json', 'bower.json');
         this.bulkCopy('.gitignore', '.gitignore');
